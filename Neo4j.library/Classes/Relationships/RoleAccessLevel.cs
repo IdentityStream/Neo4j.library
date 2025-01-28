21e77128181d7e8a59185c642533ee6d42924f2d
Dany Gonzalez-Teigland <dany@identitystream.com>
2025-01-28 14:19:15 +0100

    fixed typo in RoleAccessLevel.cs

--- Neo4j.library/Classes/Relationships/RoleAccessLevel.cs
+++ Neo4j.library/Classes/Relationships/RoleAccessLevel.cs
@@ -20,9 +20,9 @@ namespace Neo4j.library.Classes.Relationships
         {
             return
                 "UNWIND $batch AS params " +
-                "MATCH (role:Role {RoleId: params.roleId}) " +
-                "MATCH (accessLevel:AccessLevel {AccessLevelId: params.accessLevelId}) " +
-                "MERGE (role)-[r:GRANTS_ACCESS_LEVEL {RoleAccessLevelId: params.roleAccessLevelId}]->(accessLevel) " +
+                "MATCH(role:Role {RoleId: params.roleId}) " +
+                "MATCH(accessLevel:AccessLevel {AccessLevelId: params.accessLevelId}) " +
+                "MERGE(role)-[r:GRANTS_ACCESS_LEVEL {RoleAccessLevelId: params.roleAccessLevelId}]->(accessLevel) " +
                 "SET r += params.parameters ";
         }
         public override object GetParameters()
@@ -32,7 +32,7 @@ namespace Neo4j.library.Classes.Relationships
                 roleAccessLevelId = RoleAccessLevelId,
                 roleId = RoleId,
                 accessLevelId = AccessLevelId,
-                prameters = Parameters
+                parameters = Parameters
             };
         }
     }
