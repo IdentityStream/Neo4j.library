6d9e38f94e120ef1f105c1ba1dc5dc8706842a8e
Dany Gonzalez-Teigland <dany@identitystream.com>
2025-01-28 14:04:31 +0100

    Added constraint-initializer, creates constraints and indexes. Meant to be used manually at the set-up phase

---
+++ Neo4j.library/InitializeDB.cs
@@ -0,0 +1,38 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neo4j.library
+{
+    public class InitializeDB
+    {
+
+        private readonly IDriver _driver;
+
+
+        public InitializeDB(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public async Task CreateConstraints()
+        {
+            var session = _driver.AsyncSession();
+            try
+            {
+                await session.RunAsync("CREATE CONSTRAINT unique_userId FOR (u:User) REQUIRE u.UserId IS UNIQUE");
+                await session.RunAsync("CREATE CONSTRAINT unique_roleId FOR (r:Role) REQUIRE r.RoleId IS UNIQUE");
+                await session.RunAsync("CREATE CONSTRAINT unique_accessLevelId FOR (aL:AccessLevel) REQUIRE aL.AccessLevelId IS UNIQUE");
+            }
+            finally
+            {
+                if (session != null)
+                {
+                    await session.DisposeAsync();
+                }
+            }
+        }
+    }
+}
